2f9206fde1a597fc0347931b5739f0bc434d353a
rakesh gurram <rgurram09@gmail.com>
2024-02-04 17:11:55 -0600

    firstcommit

---
+++ Angularauth_API/Angularauth_API/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Angularauth_API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angularauth_API.Context
+{
+    public class AppDbContext: DbContext  //ctor 2 tabs
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+                
+        }
+
+        public DbSet<User> users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
---
+++ Angularauth_API/Angularauth_API/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Angularauth_API.Context;
+using Angularauth_API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angularauth_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj )
+        {
+            if (userObj == null) 
+                return BadRequest();
+            var user=await _authContext.users.FirstOrDefaultAsync(x=>x.Username==userObj.Username && x.Password==userObj.Password);
+            if (user == null)
+                return NotFound(new { Message = "User not Found" });
+            return Ok(new
+            {
+                Message = "login success"
+            });
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            await _authContext.users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message="User SignedUp"
+            });
+
+        }
+    }
+}
